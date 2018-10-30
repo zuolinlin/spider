74860774e911554b967ae9ee2342063fb1a17e71
baozj <bzjsky@sina.com>
2018-10-30 11:07:38 +0800

    修改默认robots规则

--- dyly_spider/settings.py
+++ dyly_spider/settings.py
@@ -23,7 +23,7 @@ NEWSPIDER_MODULE = 'dyly_spider.spiders'
 # USER_AGENT = 'dyly_spider (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 # CONCURRENT_REQUESTS = 32

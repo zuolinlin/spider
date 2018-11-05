167b57adfbc02839068afe9932a720b625ee41b6
baozj <bzjsky@sina.com>
2018-11-05 11:17:02 +0800

    调整设置了errback必须指定callback

--- run.py
+++ run.py
@@ -11,5 +11,9 @@ if __name__ == '__main__':
     crawl([
         # 'zdb_selenium',  # 投资人物(需要谷歌浏览器渲染 版本>= 65.0.3325.0)
         # 'xiniu',  # 投资人物
-        'ljz',  # 陆想汇页面数据
+        # 'ljz',  # 陆想汇页面数据
+        # '36kr_cookies',
+        # '36kr_project',
+        'ethercap_project',
+        # 'test'
     ])

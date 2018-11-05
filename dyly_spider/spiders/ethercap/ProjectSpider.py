d2cf0572241867db10c15d660ba2afa0e8e73dd0
baozj <bzjsky@sina.com>
2018-11-05 17:51:27 +0800

    易项-项目爬虫

--- dyly_spider/spiders/ethercap/ProjectSpider.py
+++ dyly_spider/spiders/ethercap/ProjectSpider.py
@@ -13,8 +13,8 @@ class ProjectSpider(BaseSpider):
     custom_settings = {
         "COOKIES_ENABLED": True,
         # "COOKIES_DEBUG": True,
-        # "AUTOTHROTTLE_ENABLED": True,
-        # "DOWNLOAD_DELAY": 2
+        "AUTOTHROTTLE_ENABLED": True,
+        "DOWNLOAD_DELAY": 2
     }
 
     name = "ethercap_project"

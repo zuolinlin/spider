4ead445399421565a6a52b70b03f81610777fb3e
baozj <bzjsky@sina.com>
2018-11-09 15:16:49 +0800

    新增萝卜爬虫执行脚本

--- bin/robo_report.bat
+++ bin/robo_report.bat
@@ -3,7 +3,7 @@ rem echo press any key to start
 rem pause
 echo running....
 cd ..
-scrapy crawl robo_report -s LOG_FILE=/logs/robo_report_.log
+scrapy crawl robo_report -s LOG_FILE=/logs/robo_report.log
 rem echo press any key to exit
 rem pause
 exit

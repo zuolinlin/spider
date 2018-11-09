94a8f73b7d8e563ee5be9f731b2d43d958b921c3
baozj <bzjsky@sina.com>
2018-11-09 14:27:07 +0800

    七牛相关调整

--- util/QiniuUtil.py
+++ util/QiniuUtil.py
@@ -17,60 +17,59 @@ urllib3.disable_warnings()
 http = urllib3.PoolManager()
 
 
-class QiniuUtil:
+def upload(url, filename, gen_folde):
+    """
+    上传网络文件
+    :param url: 网络文件地址
+    :param filename: 文件名
+    :param gen_folde: 目录类别 image、media、authent、voice、file
+    :return: 上传七牛返回的地址、文件大小
+    """
+    try:
+        if url is None or filename is None:
+            return
+        if "image".__eq__(gen_folde):
+            gen_folde = "image"
+        elif "media".__eq__(gen_folde):
+            gen_folde = "yvd"
+        elif "authent".__eq__(gen_folde):
+            gen_folde = "important"
+        elif "voice".__eq__(gen_folde):
+            gen_folde = "voice"
+        else:
+            gen_folde = "file"
+        # 生成上传 Token
+        token = q.upload_token(bucket)
+        data = http.request("GET", url).data
+        ret, info = put_data(
+            token,
+            gen_folde + "/" + time.strftime("%Y%m%d", time.localtime()) + "/" + filename,
+            data,
+            mime_type=None
+        )
+        return qiniu_url + ret['key'], round(len(data)/1024, 2)
+    except Exception as e:
+        logger.error("exception===> " + str(e))
 
-    @staticmethod
-    def upload(url, filename, gen_folde):
-        """
-        上传网络文件
-        :param url: 网络文件地址
-        :param filename: 文件名
-        :param gen_folde: 目录类别 image、media、authent、voice、file
-        :return: 上传七牛返回的地址、文件大小
-        """
-        try:
-            if "image".__eq__(gen_folde):
-                gen_folde = "image"
-            elif "media".__eq__(gen_folde):
-                gen_folde = "yvd"
-            elif "authent".__eq__(gen_folde):
-                gen_folde = "important"
-            elif "voice".__eq__(gen_folde):
-                gen_folde = "voice"
-            else:
-                gen_folde = "file"
-            # 生成上传 Token
-            token = q.upload_token(bucket)
-            data = http.request("GET", url).data
-            ret, info = put_data(
-                token,
-                gen_folde + "/" + time.strftime("%Y%m%d", time.localtime()) + "/" + filename,
-                data,
-                mime_type=None
-            )
-            return qiniu_url + ret['key'], round(len(data)/1024, 2)
-        except Exception as e:
-            logger.error("exception===> " + str(e))
 
-    @staticmethod
-    def delete(url):
-        """
-        删除指定文件
-        :param url: 牛文件地址
-        :return:
-        """
-        try:
-            if url is not None:
-                url = url.replace(qiniu_url, "")
-                ret, info = bm.delete(bucket, url)
-                print(info)
-                print(ret)
-        except Exception as e:
-            logger.error("exception===> " + str(e))
+def delete(url):
+    """
+    删除指定文件
+    :param url: 牛文件地址
+    :return:
+    """
+    try:
+        if url is not None:
+            url = url.replace(qiniu_url, "")
+            ret, info = bm.delete(bucket, url)
+            print(info)
+            print(ret)
+    except Exception as e:
+        logger.error("exception===> " + str(e))
 
 
 if __name__ == '__main__':
-    # print(QiniuUtil.upload("https://p.ishowx.com/uploads/allimg/170526/648-1F5261GH5-50.jpg", "zly6", "image"))
-    QiniuUtil.delete("https://img1.dyly.com/image/2018-10-25/zly3")
+    # print(upload("https://p.ishowx.com/uploads/allimg/170526/648-1F5261GH5-50.jpg", "zly6", "image"))
+    delete("https://img1.dyly.com/image/2018-10-25/zly3")
 
 

2e8dff57f6c096e8d81cbf4c86625fcc88154069
baozj <bzjsky@sina.com>
2018-11-09 15:50:20 +0800

    调整

--- bin/robo_report.bat
+++ bin/robo_report.bat
@@ -3,7 +3,7 @@ rem echo press any key to start
 rem pause
 echo running....
 cd ..
-scrapy crawl robo_report -s LOG_FILE=/logs/robo_report.log
+scrapy crawl robo_report -s LOG_FILE=/logs/robo_report_%date:~0,4%-%date:~5,2%-%date:~8,2%.log
 rem echo press any key to exit
 rem pause
 exit

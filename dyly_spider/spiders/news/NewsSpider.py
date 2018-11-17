6f18e172f907e8bf84292fd2126c280b2b499e39
baozj <bzjsky@sina.com>
2018-11-17 10:24:06 +0800

    新增符串转变html选择器工具类

--- dyly_spider/spiders/news/NewsSpider.py
+++ dyly_spider/spiders/news/NewsSpider.py
@@ -13,28 +13,41 @@ class NewsSpider(BaseSpider):
     def parse(self, response):
         pass
 
-    def insert_new(self, push_date, title, new_type, source, content, spider_source):
-        self.insert("""
-        INSERT INTO `spider_news` (
-          `new_id`,
-          `push_date`,
-          `title`,
-          `new_type`,
-          `source`,
-          `content`,
-          `spider_source`,
-          `modify_date`
-        ) 
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
-        """, (
-            str(uuid.uuid4()).replace("-", ""),
-            push_date,
-            title,
-            new_type,
-            source,
-            content,
-            spider_source,
-            time.localtime()
-        ))
+    def insert_new(self, out_id, push_date, title, new_type, source, content, spider_source):
+        """
+        添加数据
+        :param out_id: 外部编号
+        :param push_date: 时间
+        :param title: 标题
+        :param new_type: 新闻类型
+        :param source: 来源
+        :param content: 内容
+        :param spider_source: 爬取来源
+        :return: 最后一行编号
+        """
+        return self.insert("""
+                        INSERT INTO `spider_news` (
+                          `new_id`,
+                          `out_id`,
+                          `push_date`,
+                          `title`,
+                          `new_type`,
+                          `source`,
+                          `content`,
+                          `spider_source`,
+                          `modify_date`
+                        ) 
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+                        """, (
+                            str(uuid.uuid4()).replace("-", ""),
+                            out_id,
+                            push_date,
+                            title,
+                            new_type,
+                            source,
+                            content,
+                            spider_source,
+                            time.localtime()
+                        ))
 
 

91106e277327e7b0bb3f21c4653ef4e2e8862ee8
baozj <bzjsky@sina.com>
2018-11-21 09:45:47 +0800

    正则工具相关

---
+++ util/RegExUtil.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+import re
+
+
+def find_first(pattern, string):
+    if string is None or len(string) == 0:
+        return
+    items = re.findall(pattern, string)
+    if len(items) > 0:
+        return items[0]

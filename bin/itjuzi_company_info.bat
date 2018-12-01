2fec4b4ee149c6f1e528df5d2cdd6efa4351df75
baozj <bzjsky@sina.com>
2018-12-01 13:52:56 +0800

    IT桔子公司详情接口数据抓取-调整

--- bin/itjuzi_company_info.bat
+++ bin/itjuzi_company_info.bat
@@ -3,7 +3,7 @@ rem echo press any key to start
 rem pause
 echo running....
 cd ..
-scrapy crawl itjuzi_api_company_info -s DOWNLOAD_DELAY=1 -s CLOSESPIDER_TIMEOUT=18000 -s LOG_FILE=/logs/itjuzi_api_company_info_1-5.log
+scrapy crawl itjuzi_api_company_info -s DOWNLOAD_DELAY=1 -s CLOSESPIDER_TIMEOUT=18000 -s LOG_FILE=/logs/itjuzi_api_company_info_%date:~0,4%-%date:~5,2%-%date:~8,2%.log
 rem echo press any key to exit
 rem pause
 exit

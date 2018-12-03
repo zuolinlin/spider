4c0d6608be58994c4391c40d941793ec75b668f7
baozj <bzjsky@sina.com>
2018-12-03 17:12:51 +0800

    新增新芽-新闻相关

--- dyly_spider/spiders/news/NewSeedSpider.py
+++ dyly_spider/spiders/news/NewSeedSpider.py
@@ -44,19 +44,8 @@ class NewSeedSpider(NewsSpider):
                 priority=1,
                 callback=self.detail
             )
-        # 分页
-        # cur_page = int(response.xpath("/html/body/div[3]/div/div[1]/div[2]/div/div/div/span[@class='cur']/text()").extract_first())
-        # if cur_page == 1:
-        #     pages = int(response.xpath("/html/body/div[3]/div/div[1]/div[2]/div/div/div/a[last()]/text()").extract_first())
-        #     for page in range(2, pages+1):
-        #         yield Request(
-        #             self.list_url.format(news_type=response.meta.get("code"), page=page),
-        #             meta=response.meta,
-        #             dont_filter=True
-        #         )
 
     def detail(self, response):
-        # out_id, push_date, title, new_type, source, digest, content, source_url, spider_source
         self.insert_new(
             RegExUtil.find_first("/(\d+)", response.url),
             response.xpath('normalize-space(/html/body/div[2]/div/div[2]/div/span[@class="date"]/text())').extract_first(),

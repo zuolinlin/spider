93c11be99fc44a0bd9237b6c5c30b658e97d67dc
baozj <bzjsky@sina.com>
2018-12-08 11:53:04 +0800

    调整及新增新功能

--- util/CookieUtil.py
+++ util/CookieUtil.py
@@ -10,9 +10,10 @@ def string_to_dict(cookie):
     cookie_dict = {}
     items = cookie.split(';')
     for item in items:
-        key = item.split('=')[0].replace(' ', '')
-        value = item.split('=')[1]
-        cookie_dict[key] = value
+        if '=' in item:
+            key = item.split('=')[0].replace(' ', '')
+            value = item.split('=')[1]
+            cookie_dict[key] = value
     return cookie_dict
 
 
--- util/RegExUtil.py
+++ util/RegExUtil.py
@@ -8,3 +8,11 @@ def find_first(pattern, string):
     items = re.findall(pattern, string)
     if len(items) > 0:
         return items[0]
+
+
+def remove(pattern, string):
+    return re.sub(
+        pattern=pattern,
+        repl="",
+        string=string
+    )

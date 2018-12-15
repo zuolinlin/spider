24f9baf7b7e954dc56fb079d2150f958821edc28
zuoxiyue <zuoxiyue123@163.com>
2018-12-15 10:45:25 +0800

    "添加活动定时任务"

--- dyly_spider/spiders/active/OfweekSpider.py
+++ dyly_spider/spiders/active/OfweekSpider.py
@@ -9,4 +9,14 @@ OFweek维科网  === 行业会议
 
 
 class OfweekSpider(ActiveSpider):
-    pass
+    #  爬虫的名字 <爬虫启动时使用  scrapy crawl xiniu>
+    name = "ofweek_active"
+    # 爬取的范围，防治爬虫爬到别的网站
+    allowed_domains = ["ofweek.com"]
+    #  开始爬取的地址  按照行业分类来爬取
+    start_urls = ['https://seminar.ofweek.com/']
+
+    def parse(self, response):
+        title = response.xpath('//*[@id="hmeeting"]/div/dl/dt/a/text()').extract_first()
+        text = response.xpath('//*[@id="hmeeting"]/div/dl/dd[1]/span/text()').extract_first()
+

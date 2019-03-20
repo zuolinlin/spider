083b3f2aa6f76ccd4b8024e0a132a436e2bcb37c
zuoxiyue <zuoxiyue123@163.com>
2019-03-20 17:39:40 +0800

    "添加定时脚本"

---
+++ bin/ebrun_new.bat
@@ -0,0 +1,9 @@
+@echo off
+rem echo press any key to start
+rem pause
+echo running....
+cd ..
+scrapy crawl ebrun_new -s CLOSESPIDER_TIMEOUT=3600 -s LOG_FILE=/logs/ebrun_new%date:~0,4%-%date:~5,2%-%date:~8,2%.log
+rem echo press any key to exit
+rem pause
+exit
--- bin/pencilnews_news.bat
+++ bin/pencilnews_news.bat
@@ -3,7 +3,7 @@ rem echo press any key to start
 rem pause
 echo running....
 cd ..
-scrapy crawl pencilnews_news -s CLOSESPIDER_TIMEOUT=3600 -s LOG_FILE=/logs/pencilnews_news_%date:~0,4%-%date:~5,2%-%date:~8,2%.log
+scrapy crawl pencilnews_news -s CLOSESPIDER_TIMEOUT=3600 -s LOG_FILE=/logs/pencilnews_news%date:~0,4%-%date:~5,2%-%date:~8,2%.log
 rem echo press any key to exit
 rem pause
 exit

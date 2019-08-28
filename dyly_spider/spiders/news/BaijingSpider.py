987837be2bb2295b5ed6b6f5492f10c9975b03dd
zuoxiyue <zuoxiyue123@163.com>
2019-08-28 11:34:35 +0800

    "晨哨君"

---
+++ dyly_spider/spiders/news/BaijingSpider.py
@@ -0,0 +1,89 @@
+
+
+# -*- coding: utf-8 -*-
+import json
+import datetime
+
+from scrapy import Request
+from dyly_spider.spiders.news.NewsSpider import NewsSpider
+from util import XPathUtil, date_util
+import time
+
+class BaijingSpider(NewsSpider):
+    """
+    晨哨君
+    """
+    custom_settings = {
+        "AUTOTHROTTLE_ENABLED": True,
+        "DOWNLOAD_DELAY": 10,
+        "CONCURRENT_REQUESTS": 1
+    }
+
+    name = "baijing_news"
+    allowed_domains = ["baijingapp.com"]
+
+    start_url = "http://www.baijingapp.com"
+
+    def __init__(self, *a, **kw):
+        super(BaijingSpider, self).__init__(*a, **kw)
+
+    def start_requests(self):
+        yield Request(
+            self.start_url,
+            dont_filter=True
+        )
+
+    def parse(self, response):
+        """
+        机构列表
+        :param response:
+        :return:
+        """
+        print(response)
+        data_list =response.xpath('//div[@class="aw-mod aw-article-list"]//div[@class="articleSingle"]')
+        for data in data_list:
+            detailurl =str(data.xpath('./div[@class="articleSingle-info"]/h1/a/@href').extract_first())
+            title = data.xpath('./div[@class="articleSingle-info"]/h1/a/@title').extract_first()
+            digest = data.xpath('./div[@class="articleSingle-info"]/div/p/text()').extract_first()
+            strs = str(detailurl).split('/')
+            out_id =strs[strs.__len__() -1]
+            publishTime= datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            yield Request(
+                url=detailurl,
+                meta={"out_id": out_id,
+                      "title": title,
+                      "publishTime": publishTime,
+                      "digest": digest
+                      },
+                callback=self.detail
+            )
+
+    def detail(self,response):
+        out_id =response.meta['out_id']
+        title = response.meta['title']
+        publishTime = response.meta['publishTime']
+        digest = response.meta['digest']
+        content =response.xpath("/html/body").extract_first()
+        self.insert_new(
+            out_id,
+            publishTime,
+            title,
+            "资讯",
+            "白鲸",
+            digest,
+            content,
+            response.url,
+            5
+        )
+        print("success")
+
+    def get_data(self, req):
+        body = json.loads(req.body)
+        if body["code"] == 1:
+            data = body["data"]
+            if len(data.get("rows", [])) > 0:
+                return data
+            else:
+                self.log_error("request failed：" + repr(body))
+        else:
+            self.log_error("request failed：" + repr(body))
--- run.py
+++ run.py
@@ -10,6 +10,6 @@ def crawl(spiders):
 if __name__ == '__main__':
     crawl([
         # 'zdb_selenium',  # 投资人物(需要谷歌浏览器渲染 版本>= 65.0.3325.0)
-        'morning_whistle_news',  # 投资人物
+        'baijing_news',  # 投资人物
         # 'xiniuReport',  # 烯牛company详情页
     ])

88aada1b2f8e95a0123b9f48c64eaedcac3343da
zuoxiyue <zuoxiyue123@163.com>
2019-08-28 18:03:53 +0800

    "创投时报"

---
+++ bin/ctsb_news.bat
@@ -0,0 +1,9 @@
+@echo off
+rem echo press any key to start
+rem pause
+echo running....
+cd ..
+scrapy crawl ctsbw_news -s CLOSESPIDER_TIMEOUT=3600 -s LOG_FILE=/logs/ctsbw_news_%date:~0,4%-%date:~5,2%-%date:~8,2%.log
+rem echo press any key to exit
+rem pause
+exit
